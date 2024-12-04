0b6cdfbb49d720cb4f2d483385e3ee64b65074d2
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-12-04 14:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let btn=document.getElementById("btn");
+
+let pfunction=(delay)=>
+	{
+		return new Promise((resolve,reject)=>
+			{
+				setTimeout(()=>{resolve("resolved")},delay);
+			})
+	}
+
+btn.addEventListener('click',async()=>
+	{
+		let text=document.getElementById('text').value;
+		let delay=document.getElementById('delay').value;
+
+		let res=await pfunction(delay);
+
+		document.getElementById("output").textContent=text;
+	})
